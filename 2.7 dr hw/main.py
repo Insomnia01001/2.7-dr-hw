9a0b3b96ddeb61cd639ffa80ef4a878518b9c71e
Insomnia01001 <167763964+Insomnia01001@users.noreply.github.com>
2024-10-10 18:29:37 +0300

    Add files via upload

---
+++ 2.7 dr hw/main.py
@@ -0,0 +1,21 @@
+# mevalar= {}
+# for i in range(5):
+#     inp1 = input("maveni nomni kiriting")
+#     inp2 = int(input("narhini kiriting"))
+#     mevalar[inp1]=inp2
+# print(mevalar)    
+
+
+# dict1 = {'olma': 12000, 'banan':25000, 'gilos': 20000, 'shaftoli': 16000, 'uzum':23000} 
+# inp3= input("mevani nomini kiriting va biz narxni chaqaramiz: ")
+# for i in dict1:
+#     if i in inp3:
+#         print(dict1[i])
+# dict2 = {"Muhammadaziz":"Shokirov","Tolib":"Gofurov","Botir":"Adurahmonov","Murod":"Roziqov","Umaraliyev":"Begzod"}
+# inp4 = input("Oquvchining Ismini kiriting: ")
+# if inp4 in dict2:
+#         dict2.pop(inp4)
+#         print(dict2)
+
+# else:
+#     print("bunday oquvchi yoq")
